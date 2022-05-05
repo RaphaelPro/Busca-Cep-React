2421f78a42896a212bcbdc656d1f6161085ae125
RaphaelPro <raphaelalvesoliver998@gmail.com>
2022-05-05 13:13:08 -0300

    Adição de animação e correção de bug

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import './App.css';
-import {useState} from 'react'
+import {useState, useEffect ,useRef} from 'react'
 import SearchResults from './components/SearchResults';
 
 function App() {
   
   const [data, setData] = useState(undefined);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if(inputRef){
+      inputRef.current.focus();
+    }
+  },[])
+
 
   const handleInputChange = (e) =>{
     e.preventDefault();
 
-    //  console.log("Resultado do handle:"+ e.target.value);
-    
     const {value} = e.target;
     
-    if(!value) return;
+    if(!value){
+      setData(undefined);
+      return
+    }
 
     const url = `https://viacep.com.br/ws/${value}/json/`;
 
     fetch(url)
     .then((response) => response.json())
     .then((data) => setData(data));
-    // .then(console.log)
   };
 
   console.log('Data', data);
@@ -29,7 +37,7 @@ function App() {
     <div className='container'>
       <form>
         <label htmlFor='search'>Busque seu CEP</label>
-        <input name="search" id="search" onChange={handleInputChange}/>
+        <input name="search" id="search" onChange={handleInputChange} ref={inputRef}/>
       </form>
         <SearchResults data={data}/>
     </div>
